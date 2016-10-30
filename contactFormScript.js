407a0d8bd78489d91ac0b8bcf1991db6ac625e39
nicxcin <nicholas.beaumont21@gmail.com>
2016-10-30 13:54:30 +0000

    Cleaned up js

--- contactFormScript.js
+++ contactFormScript.js
@@ -1,18 +1,15 @@
-function pad(str){
-	if (str.length == 1) {
-		return("0" + str)
-	}else{
-		return(str)
+function pad(str) {
+	if (str.length === 1) {
+		return ("0" + str);
+	} else {
+		return (str);
 	}
 }
 
 
 function makeLink() {
 	
-	var code = pad($('#industry').val()) + pad($('#product').val()) + pad($('#type').val()) 
-	var url  = "http://www.3dlasermapping.com/testcontact/?data="
-	var output = url + code
-	
-	$('.panel-body').html(output)
+	var output = "http://www.3dlasermapping.com/testcontact/?data=" + pad($('#industry').val()) + pad($('#product').val()) + pad($('#type').val());
+	$('.panel-body').html(output);
 }
 
