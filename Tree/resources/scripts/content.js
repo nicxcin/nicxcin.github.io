f0abefd8dd0fe9983408ea95ad0a04ea9994c9b9
nicxcin <nicholas.beaumont21@gmail.com>
2017-09-17 22:32:20 +0200

    Uploading tree for backup

---
+++ Tree/resources/scripts/content.js
@@ -0,0 +1,166 @@
+var links = [
+		'/robin-mobile-mapping-system/',
+		'/sitemonitor-slopes/',
+		'/sitemonitor-slopes/sitemonitor-live/',
+		'/404/',
+		'/robin-mobile-mapping-system/robin-wings-airborne-laser-scanner/',
+		'/robin-panoramic/',
+		'/streetmapper-lidar-system/',
+		'/riegl/',
+		'/robin-mobile-mapping-system/robin-precision-survey-grade/'
+		
+]
+
+
+var data = [
+
+        {"nodeNumber":1,
+        "h2":"Let Us Help You Find the Right Product",
+        "options":[
+            {"destination":14, "title":"Capture", "prompt":"Capture data to create 3d maps or to perform analysis."},
+            {"destination":2, "title":"Monitor My Environment", "prompt":"Track change of slopes/ volumes/ rock faces and make informed decisions"}
+        ]},
+
+        {"nodeNumber":2,
+        "h2":"Tell us a little more...",
+        "options":[
+            {"destination":3, "title":"I'm not sure which product suits my needs", "prompt":"If you know what the product will <strong>need to do.</strong> but not which product to use."},
+            {"destination":9, "title":"I know what <strong>product</strong> I need ", "prompt":"If you know the <strong>specifications</strong> of the product you need."}
+        ]},
+
+        {"nodeNumber":3,
+        "h2":"I want to monitor:",
+        "options":[
+            {"destination":4, "title":"Coastal Erosion", "prompt":"Along with a scanner Sitemonitor Slopes can monitor change of a coastline"},
+            {"destination":5, "title":"Rockfall ", "prompt":"Along with a scanner Sitemonitor Slopes can monitor Rockfall across a quarry site"},
+            {"destination":5, "title":"Slope Stability ", "prompt":"Along with a scanner Sitemonitor Slopes can monitor Slope Stabilty"},
+            {"destination":7, "title":"Stockpile Management ", "prompt":"Volume measurement with Sitemonitor LIVE"},
+            {"destination":8, "title":"Underground ", "prompt":"Tracking underground with Sitemonitor underground and a ROBIN SLAM system"}
+        ]},
+            
+        {"nodeNumber":4,
+        "h2":"We recommend the Sitemonitor Slopes software:",
+        "options":[{"destination":102, "title":"Sitemonitor Slopes"}]},
+
+        {"nodeNumber":5,
+        "h2":"We recommend the Sitemonitor Slopes software:",
+        "options":[{"destination":102, "title":"Software Designed for measuring slope stability and change", "prompt":"Go to the Sitemonitor Slopes Page..."}]},
+
+        {"nodeNumber":7,
+        "h2":"We recommend the Sitemonitor Live Software:",
+        "options":[{"destination":103, "title":"For Automated Inventory Calculations", "prompt":"Go to the Sitemonitor Live Page..."}]},
+
+        {"nodeNumber":8,
+        "h2":"We Recommend Sitemonitor Underground Software",
+        "options":[{"destination":104, "title":"Track Changes in Walls or Tunnels Underground", "prompt":"Go to the Sitemonitor Underground page..."}]},
+
+        {"nodeNumber":9,
+        "h2":"Which System do you need?",
+        "options":[
+            {"destination":10, "title":"Sitemonitor SLOPES (Change)", "prompt":"Software for monitoring change including rockfall/ excavation tracking"},
+            {"destination":10, "title":"Sitemonitor SLOPES (Safety)", "prompt":"Tracking analysis of Rock Faces for safety and risk prevention."},
+            {"destination":12, "title":"Sitemonitor LIVE", "prompt":"Automatic Volume Calculations"},
+            {"destination":13, "title":"Sitemonitor UNDERGROUND", "prompt":"Tracking underground with Sitemonitor underground and a ROBIN SLAM system"}
+        ]},
+            
+        {"nodeNumber":10,
+        "h2":"We recommend the Sitemonitor SLOPES:",
+        "options":[{"destination":102, "title":"Designed for measuring change", "prompt":"Along with a scanner Sitemonitor SLOPES can accurately track coastal erosion, analyse rock faces and measure excavation."}]},
+                        
+        {"nodeNumber":12,
+        "h2":"We recommend the Sitemonitor LIVE:",
+        "options":[{"destination":103, "title":"Sitemonitor LIVE", "prompt":"Automatic Volume Calculations, Go to the Sitemonitor Live Page..."}]},
+
+        {"nodeNumber":13,
+        "h2":"We Recommend Sitemonitor UNDERGROUND",
+        "options":[{"destination":104, "title":"Sitemonitor UNDERGROUND", "prompt":"Tracking underground with Sitemonitor underground and a ROBIN SLAM system"}]},
+
+        {"nodeNumber":14,
+        "h2":"Let Us Help You Find the Right Product...",
+        "options":[
+            {"destination":15, "title":"I know what I need ", "prompt":"If you know the <strong>specifications</strong> of the product you need."},
+            {"destination":23, "title":"I know what I want to map", "prompt":"If you know what the system will <strong>need to do.</strong>"}
+        ]},
+
+        {"nodeNumber":15,
+        "h2":"Which System do you need?",
+        "options":[
+            {"destination":16, "title":"ROBIN <br> Map Everywhere", "prompt":"A 3-in-1 scanning system capable of capturing data from the air, backpack or vehicle."},
+            {"destination":21, "title":"Streetmapper <br> Map on the move ", "prompt":"Survey Roads and infrastructure with survey grade accuracy quickly by car."},
+            {"destination":22, "title":"RIEGL Scanners ", "prompt":"Distribution of RIEGL Laser Measurement Systems in the UK"}
+        ]},
+
+        {"nodeNumber":16,
+        "h2":"Options for ROBIN",
+        "options":[
+            {"destination":17, "title":"ROBIN ", "prompt":"<strong> WALK | DRIVE | FLY  </strong><br>  The Worlds Most Flexible Mobile Mapping System"},
+            {"destination":18, "title":"ROBIN Precision ", "prompt":"Map with survey Grade accuracy"},
+            {"destination":19, "title":"ROBIN +WINGS ", "prompt":"Map from the Air from a heavy lift drone or Heli with the wings pod"},
+            {"destination":20, "title":"ROBIN +PANORAMIC", "prompt":"For a 360&deg; Spherical View"}
+        ]},
+
+        {"nodeNumber":17,
+        "h2":"We recommend ROBIN",
+        "options":[{"destination":101, "title":"Map Everywhere with ROBIN", "prompt":"Go to the ROBIN page..."}]},
+
+        {"nodeNumber":18,
+        "h2":"We recommend ROBIN Precision",
+        "options":[{"destination":101, "title":"Precision", "prompt":"Go to the ROBIN Precision page..."}]},
+
+        {"nodeNumber":19,
+        "h2":"We recommend ROBIN + WINGS",
+        "options":[{"destination":105, "title":"ROBIN +WINGS", "prompt":"Go to the ROBIN +WINGS page..."}]},
+
+        {"nodeNumber":20,
+        "h2":"We recommend ROBIN + PANORAMIC",
+        "options":[{"destination":106, "title":"ROBIN +PANORAMIC", "prompt":"Go to the ROBIN +PANORAMIC page..."}]},
+
+        {"nodeNumber":21,
+        "h2":"We recommend Streetmapper IV",
+        "options":[{"destination":107, "title":"Streetmapper IV", "prompt":"High accuracy road and rail mapping"}]},
+
+        {"nodeNumber":22,
+        "h2":"RIEGL Scanners",
+        "options":[{"destination":108, "title":"RIEGL Scanners", "prompt":"Distribution of RIEGL Laser Measurement Systems in the UK And Africa"}]},
+
+        {"nodeNumber":23,
+        "h2":"I Know what I want to map",
+        "options":[
+            {"destination":25, "title":"From a Vehicle ", "prompt":"<ul><li>Roadmapping</li> <li>City Mapping</li> <li>Topographical Mapping</li> <li>Corridor mapping</li></ul>"},
+            {"destination":26, "title":"From the Air ", "prompt":"<ul><li>Hard to reach areas</li> <li>Hazardous areas</li> <li>Large Scale Mapping</li></ul>"},
+            {"destination":27, "title":"Coastal Areas ", "prompt":"<ul><li>Coastal Erosion</li> <li>Rock Face mapping</li> <li>Hard to reach coastal formations (caves)</li></ul>"},
+            {"destination":28, "title":"Pedestrian only Areas ", "prompt":"Lightweight backpack systems for use on foot <br>(Less than 14Kg)"},
+            {"destination":29, "title":"Hazardous Areas ", "prompt":"<ul><li>None Personal Areas</li> <li>UAV</li> <li>Aerial Mapping</li></ul>"}
+        ]},
+
+        {"nodeNumber":25,
+        "h2":"Mapping from a vehicle",
+        "options":[
+            {"destination":31, "title":"ROBIN ", "prompt":"DRIVE:<br> Just 1 of ROBIN'S 3 Mounting Options"},
+            {"destination":32, "title":"Streetmapper IV ", "prompt":"High accuracy road and rail mapping"}
+        ]},
+
+        {"nodeNumber":26,
+        "h2":"To map from the air we recommend ROBIN +WINGS",
+        "options":[{"destination":105, "title":"ROBIN WINGS", "prompt":"Map from a rotary or fixed wing aircraft"}]},
+        
+        {"nodeNumber":27,
+        "h2":"Coastal Mapping",
+        "options":[ {"destination":101, "title":"ROBIN ", "prompt":"Map cliff faces and shorelines via an aircraft with +WINGS"}]},
+
+        {"nodeNumber":28,
+        "h2":"Map Areas without Vehicular access",
+        "options":[{"destination":101, "title":"ROBIN ", "prompt":"A lightweight and ruggarized backpack system"}]},
+
+        {"nodeNumber":29,
+        "h2":"Mapping hazardous Areas, We recommend ROBIN +WINGS",
+        "options":[{"destination":105, "title":"ROBIN + WINGS ", "prompt":"Map from a rotary or fixed wing aircraft"}]},
+
+        {"nodeNumber":31,
+        "h2":"ROBIN + PRECISION",
+        "options":[{"destination":109, "title":"ROBIN PRECISION", "prompt":"With a fibre optic IMU for Survey-grade accuracy."}]},
+
+        {"nodeNumber":32,
+        "h2":"Streetmapper IV",
+        "options":[{"destination":107, "title":"Streetmapper IV", "prompt":"Avalible with single or dual scanners for high accuracy Road and Rail mapping  <br> Go to the Steetmapper IV page... "}]}
+];
