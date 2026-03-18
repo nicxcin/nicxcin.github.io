c24ee96ea42f888850c26f33324ed795c9d5b558
Nicholas Beaumont <nicxcin@users.noreply.github.com>
2026-03-18 12:23:52 +0100

    Update Confirm_Del_Pkg_Modal.js

--- js/modal/Confirm_Del_Pkg_Modal.js
+++ js/modal/Confirm_Del_Pkg_Modal.js
@@ -1,4 +1,3 @@
-
 import { DB } from '/js/DB.js';
 
 
@@ -26,38 +25,6 @@ export const Confirm_Del_Pkg_Modal = {
 	}
 };
 
-function _show_new() {
-
-	const modal = new bootstrap.Modal(document.getElementById('del_pkg_modal'));
-	modal.show();
-
-	document.getElementById('confirmDeleteBtn').addEventListener('click', () => {
-
-		const modalElement = document.getElementById('del_pkg_modal');
-		const modalInstance = bootstrap.Modal.getInstance(modalElement);
-		
-		if (modalInstance) {
-			console.log("Hiding Del Pkg Modal")
-			modalInstance.hide();
-		}
-
-		bootstrap.Modal.getInstance(document.getElementById('del_pkg_modal')).hide();
-
-
-		const ride_id = Router.get_ride_id()
-		const pkg = DB.get_pkg(ride_id, package_id)
-		DB.delete_package(ride_id, package_id)
-		
-	});
-
-	console.log(document.getElementById('del_pkg_modal'))
-
-	// modal.addEventListener('hidden.bs.modal', function (event) {
-	// 	console.log('Modal is fully closed. Now I can refresh the map!');
-	// 	document.querySelector('.modal-backdrop').remove();
-	// });
-	
-}
 
 function _show(ride_id, package_id) {
 
